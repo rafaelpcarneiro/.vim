2af3d2617bf8b99797cd9bb8e7d2cb2ec56c5d9c
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2021-06-16 18:34:09 -0300

    template for perl with utf8 treatment

---
+++ ftplugin/perl/template.pl
@@ -0,0 +1,20 @@
+#!/bin/perl
+# vim: set foldmethod=marker ts=4 expandtab sw=4:
+
+#|--- Documentation {{{1
+# 1}}}
+
+#|--- Modules {{{1
+use warnings;
+use strict;
+
+use DBI;                   # database
+binmode(STDOUT, ":utf-8"); # output utf8 chars
+binmode(STDIN, ":utf-8");  # read   utf8 chars
+#1}}}
+
+#|--- Functions {{{1
+# 1}}}
+
+#|--- MAIN {{{1
+# END MAIN 1}}} 
